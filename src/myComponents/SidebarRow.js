2b2c06cfa544d84cb81668d08496c6c4ce0e5e39
omsatam <62635289+omsatam@users.noreply.github.com>
2021-05-03 22:15:52 +0530

    Update SidebarRow.js

--- src/myComponents/SidebarRow.js
+++ src/myComponents/SidebarRow.js
@@ -1,7 +1,39 @@
-import React from "react";
+import React,{useEffect} from "react";
 import "./SidebarRow.css";
 
 function SidebarRow({ Icon, title, selected }) {
+  useEffect(() => {
+
+let sidebarComponents = document.getElementsByClassName("sidebarRow");
+
+  for (let i = 0; i < sidebarComponents.length; i++) {
+
+    sidebarComponents[i].addEventListener("click", function () {
+
+      let current = document.getElementsByClassName("selected{");
+
+      console.log(current);
+
+      if (current.length != 0) {
+
+        current[0].className = current[0].className.replace(
+
+          " selected",
+
+          ""
+
+        );
+
+      }
+
+      this.className += " selected";
+
+    });
+
+    }
+
+  }, []);
+  
   return <div className={`sidebarRow ${selected && "selected"}`}>
     <Icon className="sidebarRow__icon"/>
     <h2 className="sidebarRow__title">{title}</h2>
